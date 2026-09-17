a9ad05c37778bc62049cd44ddb68d84657f4ab9c
Dingle <dylanjurgens10@gmail.com>
2026-09-17 20:39:50 +0200

    feat(build): added Room type converters
     - Implemeted delimited string conversion for roomdb

---
+++ app/src/main/java/com/backlogbattlers/app/data/local/Converters.kt
@@ -0,0 +1,23 @@
+package com.backlogbattlers.app.data.local
+
+import androidx.room.TypeConverter
+
+class Converters {
+
+    //------------------------------
+    // converts a list string to a delimited string
+    @TypeConverter
+    fun fromStringList(value: List<String>): String {
+        return value.joinToString("|")
+    }
+
+    //------------------------------
+    // converts a delimited string back to a list string
+    @TypeConverter
+    fun toStringList(value: String): List<String> {
+        if (value.isEmpty()) {
+            return emptyList()
+        }
+        return value.split("|")
+    }
+}
