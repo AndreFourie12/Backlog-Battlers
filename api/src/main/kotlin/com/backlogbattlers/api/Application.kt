1ea22be21623ede45a8cdf7ade05d09879d15f39
ST10438312 <andre.4.fourie@outlook.com>
2026-09-21 11:06:40 +0200

    Merge Conflict Fix

--- api/src/main/kotlin/com/backlogbattlers/api/Application.kt
+++ api/src/main/kotlin/com/backlogbattlers/api/Application.kt
@@ -4,6 +4,11 @@ import com.backlogbattlers.api.routes.authRoutes
 import com.backlogbattlers.api.routes.gameRoutes
 import com.backlogbattlers.api.routes.healthRoutes
 import com.backlogbattlers.api.routes.libraryRoutes
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.routing.routing
 
 
 /**
